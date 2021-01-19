c7ee02c7be05228d5b1e288eebdc8552c89d01ce
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-01-19 18:42:23 +0000

    Increased max_iter.

--- classifiers.py
+++ classifiers.py
@@ -10,21 +10,21 @@ classifiers = {
                             is_regression=False,
                             parameters={"penalty":["l2"],
                                         "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                        "max_iter":[100000]}),
+                                        "max_iter":[1000000]}),
     
     "svc_linear_no_intercept":Classifier(classifier=LinearSVC,
                                          is_regression=False,                                         
                                          parameters={"fit_intercept":[False],
                                                      "penalty":["l2"],
                                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                                     "max_iter":[100000]}),
+                                                     "max_iter":[1000000]}),
     
     "svc_linear_l1":Classifier(classifier=LinearSVC,
                                is_regression=False,                               
                                parameters={"penalty":["l1"],
                                            "dual":[False],
                                            "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                           "max_iter":[100000]}),
+                                           "max_iter":[1000000]}),
 
     "svc_linear_l1_no_intercept":Classifier(classifier=LinearSVC,
                                             is_regression=False,                                            
@@ -32,37 +32,37 @@ classifiers = {
                                                         "penalty":["l1"],
                                                         "dual":[False],
                                                         "C":[0.1,1,10],
-                                                        "max_iter":[100000]}),
+                                                        "max_iter":[1000000]}),
     
     "svc_poly":Classifier(classifier=SVC,
                           is_regression=False,                          
                           parameters={"kernel":["poly"],
                                       "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
                                       "degree":[3],
-                                      "max_iter":[100000]}),
+                                      "max_iter":[1000000]}),
 
     "svc_rbf":Classifier(classifier=SVC,
                          is_regression=False,                         
                          parameters={"kernel":["rbf"],
                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                     "max_iter":[100000]}),
+                                     "max_iter":[1000000]}),
 
     "svc_sigmoid":Classifier(classifier=SVC,
                              is_regression=False,                             
                              parameters={"kernel":["sigmoid"],
                                          "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                         "max_iter":[100000]}),
+                                         "max_iter":[1000000]}),
 
     "lasso_lars":Classifier(classifier=LassoLarsCV,
                             is_regression=True,                            
                             parameters={"normalize":[False],
-                                        "max_iter":[100000]}),
+                                        "max_iter":[1000000]}),
 
     "lasso_lars_no_intercept":Classifier(classifier=LassoLarsCV,
                                          is_regression=True,                                         
                                          parameters={"normalize":[False],
                                                      "fit_intercept":[False],
-                                                     "max_iter":[100000]})
+                                                     "max_iter":[1000000]})
     
     }
 
