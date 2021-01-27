841dc8e6d260c676d0e958901bf043321821eef1
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-01-27 15:42:12 +0000

    Moved score function in here, as it's determined by the classifier.

--- classifiers.py
+++ classifiers.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.svm import SVC, LinearSVC
 from sklearn.linear_model import LassoLarsCV
 from collections import namedtuple
@@ -10,61 +11,75 @@ classifiers = {
                             is_regression=False,
                             parameters={"penalty":["l2"],
                                         "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                        "max_iter":[1000000]}),
-    
+                                        "max_iter":[100000]}),
+
     "svc_linear_no_intercept":Classifier(classifier=LinearSVC,
-                                         is_regression=False,                                         
+                                         is_regression=False,
                                          parameters={"fit_intercept":[False],
                                                      "penalty":["l2"],
                                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                                     "max_iter":[1000000]}),
+                                                     "max_iter":[100000]}),
     
     "svc_linear_l1":Classifier(classifier=LinearSVC,
-                               is_regression=False,                               
+                               is_regression=False,
                                parameters={"penalty":["l1"],
                                            "dual":[False],
                                            "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                           "max_iter":[1000000]}),
+                                           "max_iter":[100000]}),
 
     "svc_linear_l1_no_intercept":Classifier(classifier=LinearSVC,
-                                            is_regression=False,                                            
+                                            is_regression=False,
                                             parameters={"fit_intercept":[False],
                                                         "penalty":["l1"],
                                                         "dual":[False],
-                                                        "C":[0.1,1,10],
-                                                        "max_iter":[1000000]}),
+                                                        "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
+                                                        "max_iter":[100000]}),
     
     "svc_poly":Classifier(classifier=SVC,
-                          is_regression=False,                          
+                          is_regression=False,
                           parameters={"kernel":["poly"],
                                       "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
                                       "degree":[3],
                                       "max_iter":[1000000]}),
 
     "svc_rbf":Classifier(classifier=SVC,
-                         is_regression=False,                         
+                         is_regression=False,
                          parameters={"kernel":["rbf"],
                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
                                      "max_iter":[1000000]}),
+    
+    "svc_rbf_gamma":Classifier(classifier=SVC,
+                               is_regression=False,
+                               parameters={"kernel":["rbf"],
+                                           "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
+                                           "gamma":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],                                     
+                                           "max_iter":[1000000]}),
 
     "svc_sigmoid":Classifier(classifier=SVC,
-                             is_regression=False,                             
+                             is_regression=False,
                              parameters={"kernel":["sigmoid"],
                                          "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
                                          "max_iter":[1000000]}),
 
     "lasso_lars":Classifier(classifier=LassoLarsCV,
-                            is_regression=True,                            
+                            is_regression=True,
                             parameters={"normalize":[False],
                                         "max_iter":[1000000]}),
 
     "lasso_lars_no_intercept":Classifier(classifier=LassoLarsCV,
-                                         is_regression=True,                                         
+                                         is_regression=True,
                                          parameters={"normalize":[False],
                                                      "fit_intercept":[False],
                                                      "max_iter":[1000000]})
     
     }
+
+
+classification_score_function = lambda search: search.score
+regression_score_function     = lambda search: lambda X, y: np.mean(np.sign(search.predict(X) + np.random.randn(*y.shape)*1e-8).astype(int) == np.sign(y).astype(int))
+
+score_function_selector       = lambda is_regression: regression_score_function if is_regression else classification_score_function
+score_function                = {name:score_function_selector(clf.is_regression) for name, clf in classifiers.items()}
 
 if __name__ == "__main__":
     with open("classifiers.list", "w") as f:
