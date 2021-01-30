3e3b73009e0da98ffe6b4acabe380bd216fda896
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-01-30 12:09:50 +0000

    Updated to allow filtering by responsivity.

--- create_sweeps.py
+++ create_sweeps.py
@@ -13,6 +13,8 @@ parser.add_argument("--template", type=str, default="template_sweep.json",help="
 parser.add_argument("--tfirst", type=float, default=0, help="Start of the first window.")
 parser.add_argument("--tlast", type=float, default=10, help="Start of the last window.")
 parser.add_argument("--tstepmin", type=float, default=0.5, help="Minimum window step size.")
+parser.add_argument("--response_threshold", type=float, default=0, help="Response threshold for filtering glomeruli.")
+parser.add_argument("--min_resp_trials",    type=float, default=0, help="Minimum responsive trials for filtering glomeruli.")
 args = parser.parse_args()
 print(args)
 
@@ -26,6 +28,19 @@ with open(args.template, "rb") as f:
 if not os.path.isdir(sweeps_dir):
     print(f"Folder {sweeps_dir} not found, creating.")
     os.makedirs(sweeps_dir, exist_ok = True)
+
+# min_resp_trials is interpretd as a fraction if float or a number of trials if int.
+# We have to accept it as a float above, but if it's close to the int value, assume it's int
+if np.abs(args.min_resp_trials - int(args.min_resp_trials)) <1e-6:
+    print(f"Casting {args.min_resp_trials=} to ",end="")
+    args.min_resp_trials = int(args.min_resp_trials)
+    print(f"{args.min_resp_trials=}")
+    
+resp_filtering_suffix = ""
+if args.response_threshold:
+    resp_filtering_suffix += f"_rt{args.response_threshold:1.3f}"
+if args.min_resp_trials:
+    resp_filtering_suffix += f"_mrt{args.min_resp_trials}"
     
 for pairs in args.pairs.split(","):
     for window_size in args.window_size.split(","):
@@ -33,6 +48,8 @@ for pairs in args.pairs.split(","):
         tstep = max(args.tstepmin, wnd/2)
         for whiskers in args.whiskers.split(","):            
             sweep = dict(template)
+            sweep["response_threshold"] = args.response_threshold
+            sweep["min_resp_trials"]    = args.min_resp_trials
             sweep["n_seeds"]     = args.nseeds
             sweep["first_seed"]  = args.firstseed
             sweep["pairs"]       = [pairs]
@@ -40,7 +57,7 @@ for pairs in args.pairs.split(","):
             sweep["whiskers"]    = [whiskers]
             sweep["start_time"] = list(np.arange(args.tfirst, args.tlast, tstep))
             suffix = "_".join([str(args.nseeds) + "x", pairs, f"{int(wnd*1000)}ms", f"W{whiskers}"])
-            name   = args.base_name + "_" + suffix
+            name   = args.base_name + "_" + suffix + resp_filtering_suffix
 
             file_name = os.path.join(sweeps_dir, f"{name}.json")
             with open(f"{file_name}", "w") as f:
