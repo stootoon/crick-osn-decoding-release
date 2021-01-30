20dc8d063b2e7cc92b2303d0131d9e0dbe1ef956
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-01-30 11:50:10 +0000

    Added filtering by responsivity.

--- inputs.py
+++ inputs.py
@@ -15,6 +15,8 @@ dataroot     = os.path.join(os.getenv('DATA'), "tobias", dataset_name, 'unpacked
 
 from consts import data_dir
 
+Config = namedtuple('Config', 'seed n_sub shuf freq pairs whiskers window_size start_time')
+
 def load_experiments():
     from scipy.io import loadmat as loadmat_    
     loadmat  = lambda *args: loadmat_(os.path.join(dataroot, *args))
@@ -55,8 +57,6 @@ def load_experiments():
         np.save(f, {"dt":dt, "X":X, "pairs":pairs, "experiments":experiments, "ind2expr":ind2expr, "stims":valid_stims}, allow_pickle=True)
     logger.info(f"Wrote {data_file}.")
 
-Config = namedtuple('Config', 'seed n_sub shuf freq pairs whiskers window_size start_time')
-
 def validate_pairs(pairs):
     if not all([p in ["AB", "CD", "EF"] for p in pairs]):
         raise ValueError("Found invalid pairs in {pairs=}")
@@ -64,7 +64,51 @@ def validate_pairs(pairs):
 
 whisker_slice = {"yes":slice(0,6), "no":slice(6,12), "both":slice(0,12)}
 
-def generate_input_for_config(config, data_file = os.path.join(data_dir, "data.p"), baseline = [0,3], return_full = False):
+def filter_by_responsivity(Xrgt, t_all, ind_t_resp, t_baseline = [0, 3], response_threshold = 0, min_resp_trials = 0, return_full = False):
+    logger.debug("filter_by_responsivity")
+    Xrgt_bl   = np.copy(Xrgt[:,:,(t_all >= t_baseline[0]) & (t_all < t_baseline[-1])])
+
+    # Reshape the array so that I can compute stats over reps and time
+    Xgtr_bl   = Xrgt_bl.transpose([1,2,0])
+    Xg_tr_bl  = Xgtr_bl.reshape(Xgtr_bl.shape[0],-1)
+
+    # Compute the baseline stats per glomerulus
+    g_mean_bl = np.mean(Xg_tr_bl, axis=1)
+    g_std_bl  = np.std(Xg_tr_bl,  axis=1)
+    
+    # Compute the responses by averaging over the desired trials
+    Xrg_resp  = np.mean(Xrgt[:,:,ind_t_resp],axis=-1)
+
+    # Normalize the responses by what they would be assuming baseline stats.
+    # The mean is the baseline mean, but sd is scaled by the number of time points
+    # we average over to compute the responses.
+    n_resp    = len(ind_t_resp)    
+    Zrg_resp  = (Xrg_resp - g_mean_bl[np.newaxis,:])/(g_std_bl[np.newaxis,:]/np.sqrt(n_resp))
+    logger.debug(f"Z-score range: {np.min(Zrg_resp):1.3f} - {np.max(Zrg_resp):1.3f}.")
+    # Count the number of responsive reps per glomerulus
+    g_n_resp  = np.sum(np.abs(Zrg_resp) > response_threshold, axis=0)
+    logger.debug(f"Found {np.sum(g_n_resp>0)} glomeruli with at least one responsive trial.")
+
+    # Figure out how many response reps (aka trials) we need
+    n_trials  = Xrgt_bl.shape[0]
+    logger.debug(f"Received {min_resp_trials=}")
+    if type(min_resp_trials) is float:
+        min_resp_trials = int(min_resp_trials * n_trials)
+    logger.debug(f"Interpreted as {min_resp_trials=} trials.")        
+    assert 0 <= min_resp_trials <= n_trials, f"{min_resp_trials=} was not between 0 and {n_trials}"
+
+    # The responsive cells are those which responded above threshold
+    # on at least min_resp_trials trials.
+    flag_resp = g_n_resp>=min_resp_trials
+    logger.debug(f"Flagging the {np.sum(flag_resp)} glomeruli with at least {min_resp_trials} responsive trials.")
+    
+    if return_full:
+        return flag_resp, {"baseline_means":g_mean_bl, "baseline_sds":g_std_bl, "trial_responses":Xrg_resp, "trial_responses_norm":Zrg_resp, "n_responsive_trials":g_n_resp, "min_resp_trials":min_resp_trials}
+    else:
+        return flag_resp
+
+def generate_input_for_config(config, data_file = os.path.join(data_dir, "data.p"), baseline = [0,3], response_threshold = 0, min_resp_trials = 0, return_full = False):
+    logger.debug("generate_input_for_config")    
     if not os.path.exists(data_file):
         logger.debug(f"Could not find {data_file=}. Creating.")
         load_experiments()
@@ -83,12 +127,7 @@ def generate_input_for_config(config, data_file = os.path.join(data_dir, "data.p
     Xrgt = vstack([X[config.freq][stim,pair][whisker_slice[config.whiskers]] for stim in stims[config.freq] for pair in pairs])
     n_reps, n_glom, n_time = Xrgt.shape
     n_whisker = (lambda x: x.stop - x.start)(whisker_slice[config.whiskers])
-    
-    ind_glom = sorted(np.random.permutation(n_glom)[:config.n_sub])
-    logger.debug(f"Picked {config.n_sub=}/{n_glom=} glomeruli: {ind_glom=}")
 
-    X_sub = np.copy(Xrgt[:,ind_glom, :])
-    
     t = np.arange(0, n_time) * dt
     
     end_time = config.start_time + config.window_size
@@ -107,9 +146,31 @@ def generate_input_for_config(config, data_file = os.path.join(data_dir, "data.p
     
     logger.debug(f"Picked time indices {ind_t[0]} ({t[ind_t[0]]:1.3f} sec.) to {ind_t[-1]} ({t[ind_t[-1]]:1.3f} sec.) to span [{config.start_time=}, {end_time=}).")
 
-    X = np.mean(X_sub[:, :, ind_t],axis=-1)    
-    logger.debug(f"Shape of predictors: {X.shape=}")
+    # Filter glomeruli by responsivity
+    logger.debug(f"Filtering glomeruli by responsivity using {response_threshold=} and {min_resp_trials=}.")
+    flag_resp     = filter_by_responsivity(Xrgt, t, ind_t, response_threshold = response_threshold, min_resp_trials = min_resp_trials)
 
+    ind_resp_glom = np.where(flag_resp)[0]
+    n_resp_glom   = len(ind_resp_glom)
+    logger.debug(f"Found {n_resp_glom} responsive glomeruli: {list(ind_resp_glom)}")
+
+    if n_resp_glom > 0:
+        if config.n_sub > n_resp_glom:
+            logger.warning(f"Number of responsive glomeruli {n_resp_glom} < desired number of glomeruli {config.n_sub}.")
+        
+        ind_glom = sorted(np.random.permutation(ind_resp_glom)[:config.n_sub])
+        logger.debug(f"Picked {len(ind_glom)}/{n_resp_glom=} responsive glomeruli: {ind_glom=}")
+       
+        X_sub = np.copy(Xrgt[:,ind_glom, :])
+    
+        X = np.mean(X_sub[:, :, ind_t],axis=-1)    
+        logger.debug(f"Shape of predictors: {X.shape=}")
+    else:
+        logger.warning("No responsive glomeruli found.")
+        ind_glom = []
+        X = np.array([])
+        X_sub = np.array([])
+        
     y = []
     [logger.debug((y.append([-1 if "anti-corr" in stim else 1] * n_whisker), f"({stim=}, {pair=}) -> {y[-1]}")[1]) for stim in stims[config.freq] for pair in pairs]    
     y = np.array(y).flatten()
