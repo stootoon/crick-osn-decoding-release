a5ed97e28cfd33e4d797212eade03adf1f4d3dda
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-02-03 14:47:14 +0000

    Removed the '_long' classifiers as we no longer need them.

--- classifiers.py
+++ classifiers.py
@@ -7,25 +7,18 @@ from collections import namedtuple
 Classifier = namedtuple("Classifier", "classifier parameters is_regression") 
 
 classifiers = {
-    "svc_linear":Classifier(classifier=LinearSVC,
-                            is_regression=False,
-                            parameters={"penalty":["l2"],
-                                        "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                        "max_iter":[100000]}),
+    "lasso_lars_no_intercept":Classifier(classifier=LassoLarsCV,
+                                         is_regression=True,
+                                         parameters={"normalize":[False],
+                                                     "fit_intercept":[False],
+                                                     "max_iter":[1000000]}),    
 
     "svc_linear_no_intercept":Classifier(classifier=LinearSVC,
                                          is_regression=False,
                                          parameters={"fit_intercept":[False],
                                                      "penalty":["l2"],
                                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                                     "max_iter":[100000]}),
-    
-    "svc_linear_l1":Classifier(classifier=LinearSVC,
-                               is_regression=False,
-                               parameters={"penalty":["l1"],
-                                           "dual":[False],
-                                           "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                           "max_iter":[100000]}),
+                                                     "max_iter":[1000000]}),
 
     "svc_linear_l1_no_intercept":Classifier(classifier=LinearSVC,
                                             is_regression=False,
@@ -33,8 +26,7 @@ classifiers = {
                                                         "penalty":["l1"],
                                                         "dual":[False],
                                                         "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                                        "max_iter":[100000]}),
-    
+                                                        "max_iter":[1000000]}),    
     "svc_poly":Classifier(classifier=SVC,
                           is_regression=False,
                           parameters={"kernel":["poly"],
@@ -46,32 +38,7 @@ classifiers = {
                          is_regression=False,
                          parameters={"kernel":["rbf"],
                                      "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                     "max_iter":[1000000]}),
-    
-    "svc_rbf_gamma":Classifier(classifier=SVC,
-                               is_regression=False,
-                               parameters={"kernel":["rbf"],
-                                           "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                           "gamma":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],                                     
-                                           "max_iter":[1000000]}),
-
-    "svc_sigmoid":Classifier(classifier=SVC,
-                             is_regression=False,
-                             parameters={"kernel":["sigmoid"],
-                                         "C":[0.0001,0.001,0.01,0.1,1,10,100,1000,10000],
-                                         "max_iter":[1000000]}),
-
-    "lasso_lars":Classifier(classifier=LassoLarsCV,
-                            is_regression=True,
-                            parameters={"normalize":[False],
-                                        "max_iter":[1000000]}),
-
-    "lasso_lars_no_intercept":Classifier(classifier=LassoLarsCV,
-                                         is_regression=True,
-                                         parameters={"normalize":[False],
-                                                     "fit_intercept":[False],
-                                                     "max_iter":[1000000]})
-    
+                                     "max_iter":[1000000]}),    
     }
 
 
