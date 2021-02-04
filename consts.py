6a117ef9d64dcd78841d0cb11c62acbce53317da
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-02-04 11:43:19 +0000

    Added some logging to consts.py

--- consts.py
+++ consts.py
@@ -2,12 +2,15 @@ import os, sys
 import logging
 
 logging.basicConfig()
-logger = logging.getLogger("classify")
-logger.setLevel(logging.INFO)
+logger = logging.getLogger("consts.py")
+logger.setLevel(logging.DEBUG)
 
-base_dir = os.path.split(os.path.abspath(__file__))[0]
-
+base_dir   = os.path.split(os.path.abspath(__file__))[0]
 data_dir   = os.path.join(base_dir, "data")
 sweeps_dir = os.path.join(data_dir, "sweeps")
+
+logger.info("{base_dir=}")
+logger.info("{data_dir=}")
+logger.info("{sweeps_dir=}")
 
 
