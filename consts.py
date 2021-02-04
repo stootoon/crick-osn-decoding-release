820f0222e4d569c04361c366f11d6b27f24aed6e
Sina Tootoonian <sina.tootoonian@gmail.com>
2021-02-04 11:43:43 +0000

    Forgot f in f-strings.

--- consts.py
+++ consts.py
@@ -9,8 +9,8 @@ base_dir   = os.path.split(os.path.abspath(__file__))[0]
 data_dir   = os.path.join(base_dir, "data")
 sweeps_dir = os.path.join(data_dir, "sweeps")
 
-logger.info("{base_dir=}")
-logger.info("{data_dir=}")
-logger.info("{sweeps_dir=}")
+logger.info(f"{base_dir=}")
+logger.info(f"{data_dir=}")
+logger.info(f"{sweeps_dir=}")
 
 
